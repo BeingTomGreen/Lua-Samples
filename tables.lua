ba5773c36d0eba0aa3aa7c7979c92c415b254c5b
Tom Green <kingofcuddles@gmail.com>
2013-07-08 13:46:41 +0100

    Inital commit into single repo.

---
+++ tables.lua
@@ -0,0 +1,5 @@
+-- Initialise a table listing some fruits
+fruit = {[1] = "apple", [2] = "orange", [3] = "pear"}
+
+-- Print the first element of the fruit array, in this case apple
+print(fruit[1])
